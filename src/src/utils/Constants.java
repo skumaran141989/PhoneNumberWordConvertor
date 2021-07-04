93a22a98cfcb51dd66d701f4d9b5a18596c2a2c0
Sakth Kumarn Shanmuganathan <sakthikumaran.shanmuganathan@xome.com>
2021-07-04 06:03:03 +0530

    updated test cases and fixed bugs

--- src/src/utils/Constants.java
+++ src/src/utils/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
 	public final static String MISSING_DICTIONARY_OPTION = "Missing Dictionary file name Option";
 	public final static String MISSING_INPUT_OPTION = "Missing input file Option";
 	public final static String MISSING_NUMBER_TO_LETTER_STRATEGY_OPTION = "Missing strategy file Option";
+	public final static String INVALID_PHONE_NUMBER = "Invalid phone number";
 }
